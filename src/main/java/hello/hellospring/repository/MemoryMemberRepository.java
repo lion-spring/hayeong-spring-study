7d48d12a2cc93938d14875cd0aab9eebc51a8971
hayeongKo <polarpheno@icloud.com>
2022-09-13 21:03:53 +0900

    jdbc

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
 import java.util.*;
+
 
 public class MemoryMemberRepository implements MemberRepository
 {
